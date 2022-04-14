23e94fc6525e08b1f4ac7a6b6dcad8d6bb597ece
FnHafiz <farhanhafiz55@gmail.com>
2022-04-14 22:06:54 +0700

    edit character card

--- src/main/java/com/aetherwars/model/Character.java
+++ src/main/java/com/aetherwars/model/Character.java
@@ -10,9 +10,12 @@ public class Character extends Card {
   private int healthUp;
 
   public Character() {
-    this.name = "";
-    this.description = "";
+    super();
     this.type = Type.OVERWORLD;
+    this.attack = 0;
+    this.health = 0;
+    this.attackUp = 0;
+    this.healthUp = 0;
   }
 
   public Character(int id, String name, String description, String typeCard, String imagePath, int mana, Type element, int attack, int health, int attackUp, int healthUp) {
@@ -22,6 +25,38 @@ public class Character extends Card {
     this.health = health;
     this.attackUp = attackUp;
     this.healthUp = healthUp;
+  }
+
+  /* Getter */
+
+  public Type getType(){
+    return this.type;
+  }
+
+  public int getAttack(){
+    return this.attack;
+  }
+
+  public int getHealth(){
+    return this.health;
+  }
+
+  public int getAttackUp(){
+    return this.attackUp;
+  }
+
+  public int getHealthUp(){
+    return this.healthUp;
+  }
+
+  /* Setter */
+
+  public void setAttack(int attack){
+    this.attack = attack;
+  }
+
+  public void setHealth(int health){
+    this.health = health;
   }
 
   @Override
