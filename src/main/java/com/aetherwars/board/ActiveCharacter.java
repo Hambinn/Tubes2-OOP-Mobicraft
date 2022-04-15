f12c6ff43c3056fd78ea6e0a1cde8acaa2a8cfc8
Hilya Fadhilah Imania <hilyafadhilah@gmail.com>
2022-04-15 20:30:49 +0700

    BoardCard: Fix level & exp

--- src/main/java/com/aetherwars/board/ActiveCharacter.java
+++ src/main/java/com/aetherwars/board/ActiveCharacter.java
@@ -1,7 +1,10 @@
 package com.aetherwars.board;
 
+import com.aetherwars.model.Character;
+
 interface  ActiveCharacter {
+    public Character getCharacter();
     public void addExp(int addition);
-    public void levelUp();
+    public void levelUp() throws Exception;
     public boolean hasEffect(/* Class<Spell> spell */); // Misal butuh ngecek swap effect
 }
--- src/main/java/com/aetherwars/board/BoardCard.java
+++ src/main/java/com/aetherwars/board/BoardCard.java
@@ -5,10 +5,13 @@ import com.aetherwars.model.Character;
 import java.util.*;
 
 public class BoardCard implements ActiveCharacter {
+    private static final int minLevel = 1;
+    private static final int maxLevel = 10;
+
     private static Map<Integer, Integer> maxExp;
     static {
         maxExp = new HashMap<>();
-        for (int i = 1; i <= 10; i++) {
+        for (int i = minLevel; i <= maxLevel; i++) {
             maxExp.put(i, (2 * i) - 1);
         }
         maxExp = Collections.unmodifiableMap(maxExp);
@@ -30,15 +33,47 @@ public class BoardCard implements ActiveCharacter {
         this.effects = new ArrayList<>();
     }
 
-    public void levelUp() {
-        level += 1;
+    public Character getCharacter() {
+        return character;
+    }
+
+    public void levelUp() throws Exception {
+        if (!changeLevel(level + 1)) {
+            throw new Exception();
+        }
+    }
+
+    private boolean changeLevel(int newLevel) {
+        if (newLevel < minLevel || newLevel >= maxLevel) {
+            return false;
+        }
+
+        level = newLevel;
         exp = 0;
         hp = character.getHealth() + ((level - 1) * character.getHealthUp());
         atk = character.getAttack() + ((level - 1) * character.getAttackUp());
+
+        return true;
     }
 
     public void addExp(int addition) {
-        //
+        if (addition >= 0) {
+            int max = maxExp.get(level);
+            int space = max - exp;
+
+            while (addition >= space && changeLevel(level + 1)) {
+                addition -= space;
+                space = maxExp.get(level);
+            }
+        } else {
+            int space = exp;
+            while (Math.abs(addition) >= space && changeLevel(level - 1)) {
+                addition -= -space;
+                space = maxExp.get(level);
+            }
+        }
+
+        exp += addition;
     }
 
     public boolean hasEffect(/* Class<Spell> spell */) {
