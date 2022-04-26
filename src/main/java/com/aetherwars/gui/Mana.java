ba22d4ff8fd469af53447f22e848821bc0439b10
FnHafiz <farhanhafiz55@gmail.com>
2022-04-26 23:59:27 +0700

    edit mana

--- src/main/java/com/aetherwars/gui/Mana.java
+++ src/main/java/com/aetherwars/gui/Mana.java
@@ -6,17 +6,19 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 
+import com.aetherwars.player.*;
+
 public class Mana extends JPanel {
 
 	/**
 	 * Create the panel.
 	 */
-	public Mana() {
+	public Mana(Player player) {
 		setLayout(null);
 		JLabel mana1 = new JLabel("MANA", SwingConstants.CENTER);
 		mana1.setBounds(2,5,65,30);
 		mana1.setFont(new Font("Cascadia Code", 1, 14));
-		String totalMana = "0/4";
+		String totalMana = "0/" + Integer.toString(player.getMana());
 		JLabel isiMana = new JLabel(totalMana, SwingConstants.CENTER);
 	    isiMana.setBounds(2, 25, 65, 30);
 		isiMana.setFont(new Font("Cascadia Code", 1, 14));
